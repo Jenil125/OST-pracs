4568801789d061f3b17f075ced8be8de7737362b
Jenil125 <jenil.delvadiya132115@marwadiuniversity.ac.in>
2026-03-15 15:41:03 +0530

    Intial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello Open Source Lab 125");
+});
+
+app.get("/api/info", (req, res) => {
+  res.json({
+    name: "NPM Lab",
+    subject: "Open Source Technology",
+    semester: "125 B Batch"
+  });
+});
+
+app.get("/api/user", async (req, res) => {
+  const response = await axios.get("https://jsonplaceholder.typicode.com/users/1");
+  res.json(response.data);
+});
+
+app.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
